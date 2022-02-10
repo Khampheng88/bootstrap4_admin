79aaa970cb1835f11ffb3ff2cad25dc987093006
Khampheng25 <joyoivs@gmail.com>
2022-02-10 23:36:00 +0700

    Commit

--- jquery.js
+++ jquery.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
     $('.toggle-btn').click(function(){
-<<<<<<< HEAD
         $('.list-unstyled , .main-body').toggleClass('sidebar-show');
     });
 });
-
-=======
-        $('.list-unstyled,.main-body').toggleClass('sidebar-show')
-    })
-})
->>>>>>> 59accfe44f49553a686410cb26d3f0c9299cf5a9
